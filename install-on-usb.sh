ea4556286f6165b48e8f42198ec6b577ed510fca
Julio Herrera <jchbarreto@example.com>
2017-11-22 06:14:46 -0400

    Fixing some harcoded reference

--- install-on-usb.sh
+++ install-on-usb.sh
@@ -5,15 +5,15 @@ host_name=$1
 vagrant up
 
 # Move private key to local dir where can be modified by vagrant
-if [[ -f "~/vagrantsshkeys/devbox-private_key" ]]
+if [[ -f "~/vagrantsshkeys/$host_name-private_key" ]]
 then
 	echo "all fine!"  > /dev/null 2>&1
 else
 	mkdir ~/vagrantsshkeys > /dev/null 2>&1
 fi
 
-mv .vagrant/machines/devbox/virtualbox/private_key ~/vagrantsshkeys/$host_name-private_key
-ln -s ~/vagrantsshkeys/$host_name-private_key .vagrant/machines/devbox/virtualbox/private_key
+mv .vagrant/machines/$host_name/virtualbox/private_key ~/vagrantsshkeys/$host_name-private_key
+ln -s ~/vagrantsshkeys/$host_name-private_key .vagrant/machines/$host_name/virtualbox/private_key
 
 # Run vagrant up again
 vagrant up
